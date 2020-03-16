f77d40602f129f236eaed64be7cf28aab139e6a8
jzocher <jeff.zocher@gmail.com>
2020-03-16 16:32:54 -0500

    Changed templates

--- server.py
+++ server.py
@@ -10,19 +10,12 @@ def index():
 def html_page(page_name):
     return render_template(page_name)
 
-def write_to_db(data):
-    with open('db.txt', mode='a') as db:
-        email = data['email']
-        name = data['name']
-        subject = data['subject']
-        message = data['message']
-        file = db.write(f'{email},{name},{subject},{message}\n')
 
 def write_to_csv(data):
     with open('db.csv', newline='', mode='a') as db2:
         email = data['email']
         name = data['name']
-        subject = data['subject']
+        subject = data['msg_subject']
         message = data['message']
         csv_writer = csv.writer(db2, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([email,name,subject,message])
@@ -34,7 +27,7 @@ def submit_form():
         try:
             data = request.form.to_dict()
             write_to_csv(data)
-            return redirect('/thankyou.html')
+            return "Thank you! I will be in contact with you shortly"
         except:
             return 'Did not save to database'
     else:
